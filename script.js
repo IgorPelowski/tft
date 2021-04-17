330089804ff9f5e55145f32940439122438af86f
IgorPelowski <i.z.pelowski@gmail.com>
2021-04-17 14:10:54 +0200

    addded showing up menu

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const button = document.querySelector('.nav__button');
 function myClick(){
-    const comps = document.querySelector('.nav__comps');
-    comps.classList.toggle('nav__comps-menu');
+    const comps = document.querySelector('.menu');
+    comps.classList.toggle('menu__open');
     
 }
 button.addEventListener("click", myClick);
